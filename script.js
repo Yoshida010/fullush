2337688b35dbd1a284d0dfe0f07b9785dcbcc187
Yoshida010 <69901113+Yoshida010@users.noreply.github.com>
2021-10-11 16:15:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 'use strict'
 
+//テキストのカウントアップの設定
+var bar = new ProgressBar.Line(splash_text, {//id名を指定
+	strokeWidth: 0,//進捗ゲージの太さ
+	duration: 1000,//時間指定(1000＝1秒)
+	trailWidth: 0,//線の太さ
+	text: {//テキストの形状を直接指定	
+		style: {//天地中央に配置
+			position:'absolute',
+			left:'50%',
+			top:'50%',
+			padding:'0',
+			margin:'0',
+			transform:'translate(-50%,-50%)',
+			'font-size':'1.2rem',
+			color:'#fff',
+		},
+		autoStyleContainer: false //自動付与のスタイルを切る
+	},
+	step: function(state, bar) {
+		bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
+	}
+});
+
+//アニメーションスタート
+bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
+	$("#splash").delay(500).fadeOut(800);//アニメーションが終わったら#splashエリアをフェードアウト
+  document.getElementById('daiya1').classList.add('daiya1');
+  document.getElementById('daiya2').classList.add('daiya2');
+  document.getElementById('daiya3').classList.add('daiya3');
+  document.getElementById('daiya4').classList.add('daiya4');
+  document.getElementById('daiya5').classList.add('daiya5');
+  document.getElementById('daiya6').classList.add('daiya6');
+});  
+
 // ダイヤをクリックしたとき
 document.getElementById('daiya1').onclick = function(){
   //文字を非表示にする
